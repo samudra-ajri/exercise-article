1431cddece50d5e8c9f116e391d5743cff41626e
samudra-ajri <54126488+samudra-ajri@users.noreply.github.com>
2022-03-20 23:40:32 +0700

    Add category finders features

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,7 +3,7 @@ import { responseBody } from '../helpers/responseBodyHelpers.js'
 import Category from '../models/Category.js'
 
 // @desc    Create new category
-// @route   POST /api/auth/register
+// @route   POST /api/article-category
 // @access  private
 const createCategory = asyncHandler(async (req, res) => {
     const title = capitalize(req.body.title)
@@ -23,10 +23,40 @@ const createCategory = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc    Get all categories
+// @route   GET /api/article-category
+// @access  public
+const getCategories = asyncHandler(async (req, res) => {
+    const category = await Category.findAll()
+    if (category) {
+        responseBody(res, category, 'Get Categories Success')
+    } else {
+        res.status(400)
+        throw new Error('Not Found')
+    }
+})
+
+// @desc    Get category by id
+// @route   GET /api/article-category/:id
+// @access  public
+const getCategoryById = asyncHandler(async (req, res) => {
+    const { id } = req.params
+
+    const category = await Category.findOne({ where: { id } })
+    if (category) {
+        responseBody(res, category, 'Get Category Success')
+     } else {
+        res.status(400)
+        throw new Error('Not Found')
+     }
+})
+
 const capitalize = (word) => {
     return word[0].toUpperCase() + word.substring(1).toLowerCase();
 }
 
 export { 
-    createCategory
+    createCategory,
+    getCategories,
+    getCategoryById
 }
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import { protect } from '../middlewares/authMiddleware.js'
-import { createCategory } from '../controllers/categoryController.js'
+import { createCategory, getCategories, getCategoryById } from '../controllers/categoryController.js'
 
 const router = express.Router()
 
+router.route('/')
+    .get(getCategories)
+
 router.route('/create')
     .post(protect, createCategory)
+
+router.route('/:id')
+    .get(getCategoryById)
 
 export default router
